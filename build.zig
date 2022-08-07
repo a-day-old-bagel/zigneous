ede83fca9694d6996abce16e2284bfc97b3738da
a-day-old-bagel <a.day.0ld.bag3l@gmail.com>
2022-08-06 20:21:20 -0600

    switched build process to call vulkan sdk's dxc

--- build.zig
+++ build.zig
@@ -5,34 +5,44 @@ const vkgen = @import("lib/vulkan-zig/generator/index.zig");
 const zigvulkan = @import("lib/vulkan-zig/build.zig");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
+
+    const install_dat_step = b.addInstallDirectory(
+        .{ .source_dir = "dat", .install_dir = .{ .custom = "" }, .install_subdir = "bin/dat" },
+    );
+    b.getInstallStep().dependOn(&install_dat_step.step);
 
     const exe = b.addExecutable("zigneous", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
-    
-    // vulkan-zig: Create a step that generates vk.zig (stored in zig-cache) from the provided vulkan registry.
+
     const gen = vkgen.VkGenerateStep.init(b, "lib/vulkan-zig/examples/vk.xml", "vk.zig");
     exe.addPackage(gen.package);
 
-    // mach-glfw
     exe.addPackagePath("glfw", "lib/mach-glfw/src/main.zig");
     glfw.link(b, exe, .{});
-    
-    // shader resources, to be compiled using glslc
+
     const res = zigvulkan.ResourceGenStep.init(b, "resources.zig");
     res.addShader("triangle_vert", "dat/triangle.vert");
     res.addShader("triangle_frag", "dat/triangle.frag");
     exe.addPackage(res.package);
+
+    { // Compile HLSL
+        const dxc_step = b.step("dxc", "Build HLSL shaders");
+
+        // const dxc_command = makeDxcCmd(
+        //     "exampleCompute.hlsl",
+        //     "entryPoint",
+        //     "output.cs.cso",
+        //     "cs",
+        //     "PSO__GENERATE_MIPMAPS",
+        // );
+        // dxc_step.dependOn(&b.addSystemCommand(&dxc_command).step);
+
+        install_dat_step.step.dependOn(dxc_step);
+    }
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
@@ -49,4 +59,26 @@ pub fn build(b: *std.build.Builder) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
+}
+
+fn makeDxcCmd(
+    comptime input_path: []const u8,
+    comptime entry_point: []const u8,
+    comptime output_filename: []const u8,
+    comptime profile: []const u8,
+    comptime define: []const u8,
+) [9][]const u8 {
+    const shader_ver = "6_6";
+    const shader_dir = "dat/shaders/";
+    return [9][]const u8{
+        "dxc",
+        input_path,
+        "/E " ++ entry_point,
+        "/Fo " ++ shader_dir ++ output_filename,
+        "/T " ++ profile ++ "_" ++ shader_ver,
+        if (define.len == 0) "" else "/D " ++ define,
+        "/WX",
+        "/Ges",
+        "/O3",
+    };
 }
